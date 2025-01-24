1f5b056b6fe834744abae0c82ed527c15c8e87af
Crashy939 <78722591+Crashy939@users.noreply.github.com>
2025-01-24 10:34:25 -0600

    Lograr primer programa

--- CalcularDobleNumeroConBits.c
+++ CalcularDobleNumeroConBits.c
@@ -1,11 +1,32 @@
-//Calcular el doble del número usando operadores de desplazamiento de bits (operadores binarios).
 #include <stdio.h>
-int main()
+#include <string.h>
+
+void DoblarNumero(int a)
 {
-    int numero, resultado;
-    printf("Pon un número: ");
-    scanf("%i", &numero);
-    resultado = numero << 1;
+    int resultado;
+    resultado = a << 1;
     printf("El doble del número es: %i\n", resultado);
+}
+
+int main()
+{   
+    char repetir[3];
+    int numero;
+
+    do
+    {
+        printf("Pon un número: ");
+        scanf("%i", &numero);
+
+        // Consumir el salto de línea sobrante después de scanf
+        getchar();  // Esto lee el salto de línea pendiente
+
+        DoblarNumero(numero);
+        printf("¿Desea repetir? (S/s para sí, cualquier otra tecla para no): ");
+        fgets(repetir, sizeof(repetir), stdin);
+        repetir[strcspn(repetir, "\n")] = 0; // Eliminar el salto de línea
+
+    } while((strcmp(repetir, "S") == 0) || (strcmp(repetir, "s") == 0));
+
     return 0;
 }
